816a1887a9d22c403c4d357dd0753a26edc91254
alexis villaraza <alexis.borja@gmail.com>
2020-02-27 13:01:26 -0700

    added now.json file

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -27,7 +27,6 @@ class Login extends React.Component {
       .post('/api/login', this.state.credentials)
       .then(res => {
         window.localStorage.setItem('token', res.data.payload);
-        // navigate the user to /protected (whatever landing page)
         this.props.history.push('/protected');
       })
       .catch(err => console.log(err));
